ace547f9faeeeff8d08ffe485e631e26ad32eb6e
Richard Dodd <richard.o.dodd@gmail.com>
2022-03-28 16:05:52 +0100

    Run rustfmt.
    Also support smartstring

--- src/routing.rs
+++ src/routing.rs
@@ -1,14 +1,13 @@
 use std::borrow::Cow;
 
-use web_sys::{EventTarget, HtmlElement};
-use once_cell::sync::Lazy;
 use futures_signals::signal::{Mutable, ReadOnlyMutable};
+use once_cell::sync::Lazy;
+use web_sys::{EventTarget, HtmlElement};
 
 use crate::bindings;
 use crate::dom::{Dom, DomBuilder, EventOptions};
-use crate::utils::EventListener;
 use crate::events;
-
+use crate::utils::EventListener;
 
 // TODO inline ?
 fn change_url(mutable: &Mutable<String>) {
@@ -23,7 +22,6 @@ fn change_url(mutable: &Mutable<String>) {
     }
 }
 
-
 struct CurrentUrl {
     value: Mutable<String>,
 }
@@ -34,28 +32,28 @@ impl CurrentUrl {
         let value = Mutable::new(String::from(bindings::current_url()));
 
         // TODO clean this up somehow ?
-        let _ = EventListener::new(bindings::window_event_target(), "popstate", &EventOptions::default(), {
-            let value = value.clone();
-            move |_| {
-                change_url(&value);
-            }
-        });
+        let _ = EventListener::new(
+            bindings::window_event_target(),
+            "popstate",
+            &EventOptions::default(),
+            {
+                let value = value.clone();
+                move |_| {
+                    change_url(&value);
+                }
+            },
+        );
 
-        Self {
-            value,
-        }
+        Self { value }
     }
 }
 
-
 static URL: Lazy<CurrentUrl> = Lazy::new(|| CurrentUrl::new());
-
 
 #[inline]
 pub fn url() -> ReadOnlyMutable<String> {
     URL.value.read_only()
 }
-
 
 // TODO if URL hasn't been created yet, don't create it
 #[inline]
@@ -66,12 +64,13 @@ pub fn go_to_url(new_url: &str) {
     change_url(&URL.value);
 }
 
-
 #[deprecated(since = "0.5.1", note = "Use the on_click_go_to_url macro instead")]
 #[inline]
 pub fn on_click_go_to_url<A, B>(new_url: A) -> impl FnOnce(DomBuilder<B>) -> DomBuilder<B>
-    where A: Into<Cow<'static, str>>,
-          B: AsRef<EventTarget> {
+where
+    A: Into<Cow<'static, str>>,
+    B: AsRef<EventTarget>,
+{
     let new_url = new_url.into();
 
     #[inline]
@@ -83,15 +82,16 @@ pub fn on_click_go_to_url<A, B>(new_url: A) -> impl FnOnce(DomBuilder<B>) -> Dom
     }
 }
 
-
 // TODO better type than HtmlElement
 // TODO maybe make this a macro ?
 #[deprecated(since = "0.5.1", note = "Use the link macro instead")]
 #[allow(deprecated)]
 #[inline]
 pub fn link<A, F>(url: A, f: F) -> Dom
-    where A: Into<Cow<'static, str>>,
-          F: FnOnce(DomBuilder<HtmlElement>) -> DomBuilder<HtmlElement> {
+where
+    A: Into<Cow<'static, str>>,
+    F: FnOnce(DomBuilder<HtmlElement>) -> DomBuilder<HtmlElement>,
+{
     let url = url.into();
 
     html!("a", {
@@ -101,17 +101,19 @@ pub fn link<A, F>(url: A, f: F) -> Dom
     })
 }
 
-
 // TODO test this
 #[macro_export]
 macro_rules! on_click_go_to_url {
     ($this:ident, $url:expr) => {{
         let url = $url;
 
-        $this.event_with_options(&$crate::EventOptions::preventable(), move |e: $crate::events::Click| {
-            e.prevent_default();
-            $crate::routing::go_to_url(&url);
-        })
+        $this.event_with_options(
+            &$crate::EventOptions::preventable(),
+            move |e: $crate::events::Click| {
+                e.prevent_default();
+                $crate::routing::go_to_url(&url);
+            },
+        )
     }};
 }
 
